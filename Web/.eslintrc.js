2fc7adc9734b3745b10ba1b04a8979898b06cb65
JasonPattle <jasonpattle90@gmail.com>
2021-02-22 20:04:02 +0000

    Updated eslint to required version and removed old cypress dependencies

--- Web/.eslintrc.js
+++ Web/.eslintrc.js
@@ -1,39 +1,37 @@
 module.exports = {
-  'env': {
-      'browser': true,
-      'jest': true,
-      'es6': true,
-      'node': true
+  env: {
+    browser: true,
+    jest: true,
+    es6: true,
+    node: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'eslint:recommended',
-    'prettier'
-  ],
-  'plugins': [
-      'prettier',
-      'cypress'
-  ],
-  'rules': {
+  extends: ['plugin:react/recommended', 'eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
     'react/prop-types': 'off',
     'react/jsx-filename-extension': 'off',
     'jsx-a11y/click-events/have-key-events': 'off',
-    'padding-line-between-statements': ['error', {
-      'blankLine': 'any', 'prev': 'block-like', 'next': 'return' 
-    }],
-    'prettier/prettier': 'error',
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'indent': ['error', 2],
-    'no-multi-spaces': ['error']
-  },
-  'parser': 'babel-eslint',
-  'parserOptions': {
-      'ecmaFeatures': {
-          'jsx': true
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'any',
+        prev: 'block-like',
+        next: 'return',
       },
-      'allowImportEverywhere': true,
-      'sourceType': 'module'
+    ],
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+    'no-multi-spaces': ['error'],
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    allowImportEverywhere: true,
+    sourceType: 'module',
   },
   settings: {
     react: {
